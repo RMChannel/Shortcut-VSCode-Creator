0881e39d54403f1c9a712f3fd125217b2bc1ab37
RMChannel <98057423+RMChannel@users.noreply.github.com>
2024-05-01 13:50:04 +0200

    Versione Mac completata

--- main.c
+++ main.c
@@ -11,25 +11,14 @@ int system(const char *command);
 
 int main(int argc, char *argv[]) {
     char radice[1000]="";
-    char radiceFin[2000]="";
     char *radiceTemp=radice;
     printf("Inserisci la locazione della cartella da aprire in VSCode: ");
     scanf("%[^\n]",radice);
-    if (strlen(radice)<2 || (radice[1]!=':' && radice[2]!=':')) error("Radice non corretta\n");
-    for (int i=0;*radiceTemp;radiceTemp++) {
-        if (*radiceTemp=='"') radiceTemp++;
-        if (*radiceTemp=='\\') {
-            radiceFin[i]='\\';
-            i++;
-        }
-        radiceFin[i]=*radiceTemp;
-        i++;
-    }
-    printf("%s",radiceFin);
+    if (strlen(radice)<7 || radice[0]!='/') error("Radice non corretta\n");
     FILE *fp=fopen("script.c","w");
-    fprintf(fp,"#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n\nint system(const char *command);\n\nint main(void) {\nsystem(\"code %s\");\nreturn 0;\n}\n",radiceFin);
+    fprintf(fp,"#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n\nint system(const char *command);\n\nint main(void) {\nsystem(\"code %s\");\nreturn 0;\n}\n",radice);
     fclose(fp);
-    system("gcc script.c -o codeBello.exe");
+    system("gcc script.c -o codeBello.out");
     system("rm -f script.c");
     return 0;
 }
